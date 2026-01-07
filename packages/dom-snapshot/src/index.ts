447a2d0611a854767775a3a279421617ec3fa96a
jk4235 <835633812@qq.com>
2026-01-07 16:42:16 +0800

    feat(dom-snapshot): add DOM snapshot utility with serialization and search capabilities

---
+++ packages/dom-snapshot/src/index.ts
@@ -0,0 +1,4 @@
+export { collectDomSnapshot, collectDomSnapshotInPage } from "./collector";
+export { DomSnapshotManager } from "./manager";
+export { searchSnapshotText } from "./query";
+export * from "./types";
